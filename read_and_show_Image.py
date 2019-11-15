e25faf2b67dac3b26868f0e545138fb8ef184183
Mandeel <36323672+Mandeel@users.noreply.github.com>
2019-11-15 14:01:12 +0300

    update

--- read_and_show_Image.py
+++ read_and_show_Image.py
@@ -1,25 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-A module is a Python file containing Python statements and definitions. For example, a file evenodd.py 
-is a module, and we call it ‘evenodd’. We put similar code together in one module. 
-This helps us modularize our code, and make it much easier to deal with. 
-And not only that, a module grants us reusability. With a module, 
-we don’t need to write the same code again for a new project that we take up.
-
-A package, in essence, is like a directory holding subpackages and modules. 
-While we can create our own packages, we can also use one from the Python Package Index (PyPI) 
-to use for our projects.
-"""
-
-
 #pre-built OpenCV packages for Python
 # https://docs.opencv.org/3.4.7/d1/dfb/intro.html
 import cv2
 
 # https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
 # https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga61d9b0126a3e57d9277ac48327799c80
-imgGray = cv2.imread('poster.jpg', cv2.IMREAD_GRAYSCALE)
-imgColor = cv2.imread('poster.jpg', cv2.IMREAD_COLOR)
+imgGray = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
+imgColor = cv2.imread('image.jpg', cv2.IMREAD_COLOR)
 
  
 #https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html#imshow
@@ -36,3 +22,12 @@ cv2.waitKey(0)
 
 # https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html#destroyallwindows
 cv2.destroyAllWindows()
+
+
+cropped = imgGray[0:160,0:160]
+
+cv2.imshow('Cropped Image',cropped)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite(r"D:\Diamond\Academic\Others\Digital-Image-Processing\cropped.jpg", cropped)

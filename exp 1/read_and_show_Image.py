6986480ac3db5e8dd74d3900d5aef54d33af151a
Mandeel <36323672+Mandeel@users.noreply.github.com>
2019-11-20 09:52:02 +0300

    update

--- exp 1/read_and_show_Image.py
+++ exp 1/read_and_show_Image.py
@@ -23,11 +23,37 @@ cv2.waitKey(0)
 # https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html#destroyallwindows
 cv2.destroyAllWindows()
 
+cropped = imgGray[160:260,160:260]
+img_rotate_90_clockwise = cv2.rotate(cropped, 0)
+img_rotate_90_counterclockwise = cv2.rotate(cropped, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-cropped = imgGray[0:160,0:160]
+# https://docs.opencv.org/2.4/modules/core/doc/operations_on_arrays.html#void%20flip(InputArray%20src,%20OutputArray%20dst,%20int%20flipCode)
+flipped = cv2.flip(imgColor,-1)
+# 0 means flipping around the x-axis 
+# 1 means flipping around the y-axis 
+# -1(or any negative value) means flipping around both axes
+flipped = cv2.flip(flipped,-1)
+
 
 cv2.imshow('Cropped Image',cropped)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-cv2.imwrite(r"D:\Diamond\Academic\Others\Digital-Image-Processing\cropped.jpg", cropped)
+
+cv2.imshow('rotated Image',img_rotate_90_clockwise)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+distorted = (img_rotate_90_clockwise + img_rotate_90_counterclockwise)/2
+
+brighted = cropped * 1.5
+darkened = cropped * 0.5
+
+cv2.imwrite("cropped.jpg", cropped)
+cv2.imwrite("rotated.jpg", img_rotate_90_clockwise)
+cv2.imwrite("countered_rotated.jpg", img_rotate_90_counterclockwise)
+cv2.imwrite("flipped.png", flipped)
+cv2.imwrite("averaged.png", distorted)
+cv2.imwrite("brighted.png", brighted)
+cv2.imwrite("darkened.png", darkened)
+
